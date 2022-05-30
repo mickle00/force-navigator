8bcf7f62fb314288c03f60849e55959638070099
Danny Summerlin <danny.summerlin@jstart.org>
2022-05-29 22:22:32 -0400

    cleaning unneeded stubs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -452,20 +452,5 @@ var sfnav = (()=>{
 			}
 		} catch(e) { if(debug) console.log(e) }
 	}
-	function saveSetting(setting) {
-        chrome.storage.sync.set({
-            favoriteColor: color,
-            likesColor: likesColor
-          }, function() {
-            // Update status to let user know options were saved.
-            var status = document.getElementById('status');
-            status.textContent = 'Options saved.';
-            setTimeout(function() {
-              status.textContent = '';
-            }, 750);
-          });
-	}
-	function getSetting(setting) {}
-
 	init()
 })()
